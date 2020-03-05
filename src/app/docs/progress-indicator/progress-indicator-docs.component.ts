444ecf2b2bb03f276c42e93bd6f5668a9e3a88da
Denise Pena <denise.pena@blackbaud.me>
2020-03-05 11:04:46 -0600

    Added content for passive and waterfall indicators

--- src/app/docs/progress-indicator/progress-indicator-docs.component.ts
+++
@@ -1,9 +0,0 @@
-import {
-  Component
-} from '@angular/core';
-
-@Component({
-  selector: 'app-progress-indicator-docs',
-  templateUrl: './progress-indicator-docs.component.html'
-})
-export class ProgressIndicatorDocsComponent { }
