906c6fe1d10c944011ffa053dee678791ea3ea18
Paul Crowder <paul.crowder@blackbaud.com>
2021-09-30 09:47:41 -0400

    Merge 5.0.0 next (#76)
    * initial commit
    
    * 5.0.0-beta.0 release (#68)
    
    * 5.0.0-beta.0 release
    
    * changelog
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    
    * Enums to strings (#69)
    
    * Enums to strings
    
    * Only pass new supported string values to internal components
    
    * Docs fixes
    
    * PR feedback
    
    * Release 5.0.0-beta.1 (#70)
    
    * Adopt Angular CLI (#71)
    
    * Build #skyux-progress-indicator-push-1198248469-64885: Added new baseline screenshots. [ci skip]
    
    * Release 5.0.0-beta.2 (#72)
    
    * Use e2e theme switcher for visual tests (#73)
    
    * Use e2e theme switcher for visual tests
    
    * Update dependencies and cleanup
    
    * 5.0.0-beta.3 release; Upgrade dependencies (#74)
    
    * upgrade dependencies
    
    * npm install
    
    * update deps
    
    * changelog
    
    * typo
    
    * revert
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    
    * Update dependencies
    
    * Delete screenshots
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>
    Co-authored-by: Alex <alex.kingman@blackbaud.com>
    Co-authored-by: Blackbaud Sky Build User <sky-build-user@blackbaud.com>

---
+++ projects/progress-indicator-showcase/src/app/visual/visual.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+
+import { SkyE2eThemeSelectorModule } from '@skyux/e2e-client';
+
+import { SkyModalModule } from '@skyux/modals';
+import { SkyPopoverModule } from '@skyux/popovers';
+
+import { SkyProgressIndicatorModule } from 'projects/progress-indicator/src/public-api';
+
+import { ProgressIndicatorVisualComponent } from './progress-indicator/progress-indicator-visual.component';
+import { ProgressIndicatorWizardVisualComponent } from './progress-indicator/progress-indicator-horizontal-visual.component';
+
+import { VisualComponent } from './visual.component';
+
+@NgModule({
+  declarations: [
+    ProgressIndicatorVisualComponent,
+    ProgressIndicatorWizardVisualComponent,
+    VisualComponent
+  ],
+  imports: [
+    NoopAnimationsModule,
+    RouterModule,
+    SkyE2eThemeSelectorModule,
+    SkyModalModule,
+    SkyPopoverModule,
+    SkyProgressIndicatorModule
+  ]
+})
+export class VisualModule { }
